5e7ae06ba0b31d54142b83e77d583fc589b2589b
pogi-tangkad <pogi.tangkad@gmail.com>
2025-03-17 06:02:43 -0500

    create initial angular files in 03-frontend folder

--- 02-backend/spring-boot-ecommerce/src/main/java/com/pogi_tangkad/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/pogi_tangkad/ecommerce/dao/ProductRepository.java
@@ -4,5 +4,6 @@ package com.pogi_tangkad.ecommerce.dao;
 import com.pogi_tangkad.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
